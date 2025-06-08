239041b8681b37d04f578bc7bbeb1cd0d0ee8a41
Philipp Körner <p.koerner@hhu.de>
2025-06-08 08:48:50 +0200

    add clojars info

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject lisb-rodin-plugin-adapter "0.1.0-SNAPSHOT"
+(defproject org.clojars.pkoerner/lisb-rodin-plugin-adapter "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://repo.clojars.org/"}]
+                        ["snapshots" {:sign-releases false :url "https://repo.clojars.org/"}]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojars.pkoerner/lisb "0.0.6-SNAPSHOT"]
                  ]
